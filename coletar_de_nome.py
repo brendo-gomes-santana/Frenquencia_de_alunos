616cf0294c9050833ab700da1fb73ea1767512d7
Brendo Gomes <brendogs2021@gmail.com>
2022-03-03 21:54:56 -0300

    colocando o ensino, e nome do professor

--- coletar_de_nome.py
+++ coletar_de_nome.py
@@ -1,10 +1,12 @@
+from distutils.cmd import Command
 from tkinter import *
 from socket import SO_USELOOPBACK
 from turtle import width
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+import pandas as pd
 
-#PARTE DE PROGRAMaÇÃO
+#PARTE DE PROGRAMaÇÃO________________________________________________________________________________________________
 def visualização_de_nomes():
     url = entrada_do_url.get()
 
@@ -14,25 +16,44 @@ def visualização_de_nomes():
 
     soup = BeautifulSoup(html_lista_de_aluno, 'html.parser')
 
-    for lista_de_alunos in soup.find_all("td", {"width": "300"},{"class": "borda_tabela"}):
-        print(lista_de_alunos.get_text())
+#nome do disciplina e data do exame-----------------------------------------------------------
+    disciplina_exame = soup.find_all("div",{"class": "arial13"}, limit= 2)
 
-#PARTE VISUAL
+#qual é o ensino(médio ou fundamental)-----------------------------------------------------------
+    ensino = soup.find_all("td",{"class": "arial13"})
+
+#nome dos alunos ----------------------------------------------------------------------------
+    nomes = soup.find_all("td", {"width": "300"},{"class": "borda_tabela"})
+
+#CRIANDO PASTA NO EXCEL________________________________________________________________________________________________
+    lista_de_frenquencias = {}
+    for dis, ens, nome in zip (disciplina_exame, ensino, nomes):
+        Disciplina = dis.get_text()
+        Ensino = ens.get_text()
+        Nome = nome.get_text()
+    
+#PARTE VISUAL INICIAL___________________________________________________________________________________________________
 janela = Tk()
-janela.geometry('240x200')
+janela.geometry('560x200')
 janela.title('Frenquencia de alunos - CIEC')
 
+#mensagem-----------------------------------------------------------
 texto_orientação = Label(janela, text='Coloque o endereço da Página')
 texto_orientação.grid(column=1, row=0)
 
+#URL----------------------------------------------------------------
 url = Label(janela, text='URL:')
 url.grid(column=0, row=1)
 
-entrada_do_url = Entry(janela, width=30)
+#entrada do enderenço ----------------------------------------------
+entrada_do_url = Entry(janela, width=87)
 entrada_do_url.grid(column=1, row=1)
 
+#botão-------------------------------------------------------------
 botão = Button(janela, text='pronto', command=visualização_de_nomes)
 botão.grid(column=1, row=2)
 
 janela.mainloop()
+
+
 
